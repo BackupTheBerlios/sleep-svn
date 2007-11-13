623f77137c0a68f86dbb68b56edba2e6cd79dfda
rsmudge <rsmudge@3f375905-abf6-0310-9703-f44371ea9094>
2007-11-13 00:58:42 +0000

    ...

--- sleep/readme.txt
+++ sleep/readme.txt
@@ -1,5 +1,5 @@
  ------- -------------------- ---------   ---------------  ------- ---------
-Sleep - 2.1 beta 19 - README   
+Sleep - 2.1 beta 20 - README   
 -  ------------- -------- -   ---------------- --       -    -     -     - - -
 
 "You got the language, all you need now is the O'Reilly book".  That is
--- sleep/src/sleep/runtime/SleepUtils.java
+++ sleep/src/sleep/runtime/SleepUtils.java
@@ -26,7 +26,7 @@ import sleep.bridges.*;
 public class SleepUtils
 {
    /** A date stamp of this Sleep release in YYYYMMDD format */
-   public static final int    SLEEP_RELEASE = 20071024;
+   public static final int    SLEEP_RELEASE = 20071112;
 
    /** A string description of this Sleep release */
    public static final String SLEEP_VERSION = "Sleep 2.1";
